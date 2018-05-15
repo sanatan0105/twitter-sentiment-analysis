e9dc04a446b06f4589b4cb6acadfe7e2bb396a47
Sanatan <sanatan0105@gmail.com>
2018-05-15 17:16:40 +0530

    Add files via upload

---
+++ projects/hidden.py
@@ -0,0 +1,5 @@
+def oauth():
+    return {"consumer_key": "UwHh9p3tf3KPdY0XNHcTysq5Z",
+            "consumer_secret": "xyBLAu4lLXrR3lYTdvzg1MC3VUDwFSk3mda07hpcLYhvYlbg1i",
+            "token_key": "945067566-mG3UykrUXjBvP0rufpPuXipcuKHThFqk6AnMINeL",
+            "token_secret": "jetvClgbt0tkmc5ueggoV9ZFOmoSS7RU7MxXxmPY4yJAW"}
